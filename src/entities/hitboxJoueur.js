7c5a4c0dffde401c2ca72a646762aab62b030e5c
PIERREHugo <1hugopierre1@gmail.com>
2023-05-17 13:06:09 +0200

    z

--- src/entities/hitboxJoueur.js
+++ src/entities/hitboxJoueur.js
@@ -1,8 +1,11 @@
 export default class HitBoite extends Phaser.Physics.Arcade.Sprite{
     constructor(scene, x, y, texture){
         super(scene, x, y, texture)
-        
+        this.clavier = scene.input.keyboard.addKeys('A,Z,SPACE,E');
+        this.callMe = false
         scene.physics.world.enable(this)
         scene.add.existing(this)
     }
+
+
 }
