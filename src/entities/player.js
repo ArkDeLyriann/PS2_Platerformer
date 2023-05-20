61f06832d33f3a9482f582f8362f9e991559c8b8
ArkDeLyriann <1hugopierre1@gmail.com>
2023-05-20 22:05:31 +0200

    aaaa

--- src/entities/player.js
+++ src/entities/player.js
@@ -2,7 +2,7 @@ export default class Player extends Phaser.Physics.Arcade.Sprite{
     constructor(scene, x, y, texture){
         super(scene, x, y, texture)
         this.cursors = scene.input.keyboard.createCursorKeys();
-        this.clavier = scene.input.keyboard.addKeys('A,Z,SPACE,E');
+        this.clavier = scene.input.keyboard.addKeys('A,Z,SPACE,E,R');
         this.canDash = true
         this.canMove = true
         this.regardeBas = false
@@ -76,32 +76,52 @@ export default class Player extends Phaser.Physics.Arcade.Sprite{
                 
                 
             } 
-            if (Phaser.Input.Keyboard.JustDown(this.cursors.down)){
-                this.regardeBas = true
+            
+            else if (this.cursors.down.isDown) {
+                mouvement.y = 1;
+
+                
+                
+            }else if (this.cursors.up.isDown) {
+                console.log("baise")
+                mouvement.y = -1;
+
+                
+                
+            } 
+            else {
+                mouvement.y = 0;
                 this.goingLeft = false
                 this.goingRight = false
+                
             }
             
             mouvement.normalize();
-            this.setVelocityX(mouvement.x * 300);
+            this.setVelocity(mouvement.x * 300, mouvement.y * 300 );
+            
+            
         }
         
-        
-        
-        if (Phaser.Input.Keyboard.JustDown(this.cursors.up)) {
+        if (Phaser.Input.Keyboard.JustDown(this.clavier.R)) {
             if (this.canFly){
                 this.isFlying = true
-                this.setVelocityY(-700);
+                this.canMove = false
+                console.log(this.isFlying)
                 this.body.setAllowGravity(false)
             }
-            else if(this.body.blocked.down){
+
+
+        }  
+        
+        if (Phaser.Input.Keyboard.JustDown(this.cursors.up)) {
+            if(this.body.blocked.down){
                 this.isJumping = true
-                this.setVelocityY(-700);
+                this.setVelocityY(-550);
             }
             else if (this.canJump){
                 this.isJumping = true
                 this.canJump = false;
-                this.setVelocityY(-700);
+                this.setVelocityY(-550);
             }
         }   
         
@@ -137,7 +157,7 @@ export default class Player extends Phaser.Physics.Arcade.Sprite{
                 this.canFly = false
                 this.body.setAllowGravity(true)
                 this.isFlying = false
-            }, 4000);
+            }, 30000);
         }
         
         
