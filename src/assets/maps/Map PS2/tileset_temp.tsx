002442ef0ad90959f4ed9af49a8ad00b8dd805d8
ArkDeLyriann <1hugopierre1@gmail.com>
2023-06-02 10:28:27 +0200

    tiled

---
+++ src/assets/maps/Map PS2/tileset_temp.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tileset_temp" tilewidth="64" tileheight="64" tilecount="324" columns="18">
+ <image source="../Nouveau dossier (4)/PS2_Platerformer/src/assets/maps/tileset_temp.png" width="1200" height="1200"/>
+</tileset>
